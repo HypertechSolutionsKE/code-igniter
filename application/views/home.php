3c0dfec25deeb95fa90314deb9b888221a04a2d3
ngigi-nyoro <nyorojnr@gmail.com>
2017-03-18 19:36:50 +0300

    configuring code igniter for the first time

---
+++ application/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/styles.css">
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Home Page</h1>
+
+</body>
+</html>
